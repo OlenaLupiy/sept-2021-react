0192becf8d2d6245a268fd47c5a3071252e49d11
OlenaLupiy <ollupiy@gmail.com>
2022-02-06 17:15:48 +0200

    class2_repeat_done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-export default function App(){
-    
+import React, {useEffect, useState} from 'react';
+import {userService} from "./services/user.service";
+import Form from "./components/Form/Form";
+import Users from "./components/Users/Users";
+
+const App = () => {
+    const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    useEffect(()=>{
+        userService.getAll().then(value=> {
+            setUsers([...value])
+            setFilteredUsers([...value])
+        })
+    }, [])
+
+    const getFilter = (data) => {
+        let filteredArr = [...users];
+        if (data.name){
+            filteredArr = filteredArr.filter(user=>user.name.toLowerCase().includes(data.name.toLowerCase()))
+        }if (data.username){
+            filteredArr = filteredArr.filter(user=>user.username.toLowerCase().includes(data.username.toLowerCase()))
+        }
+        if (data.email) {
+
+            filteredArr = filteredArr.filter(user => user.email.toLowerCase().includes(data.email.toLowerCase()))
+        }
+        setFilteredUsers(filteredArr)
+
+    }
     return (
         <div>
-            
+            <Form getFilter={getFilter}/>
+            <Users users={filteredUsers} />
+
         </div>
-    )
-}
+    );
+};
+
+export default App;
